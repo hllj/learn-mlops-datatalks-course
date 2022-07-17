3095b0ab1f6b154a6da0d53cf6ba173f831d5c5b
hllj <vanhop3499@gmail.com>
2022-07-17 16:42:54 +0700

    update prefect main flow and download dataset

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 from .process_data import process_data
 from .train import train
+from .download_dataset import download_dataset_by_date
--- workflow.py
+++ workflow.py
@@ -3,13 +3,13 @@ from prefect import task, flow, get_run_logger
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-from src import process_data, train
+from src import download_dataset_by_date, process_data, train
 
 RAW_DATA_PATH = './data'
 DEST_PATH = './output'
 
 @task
-def get_paths(date: str, raw_data_path: str, dataset: str):
+def get_paths(date: str, dataset: str):
     if date:
         processed_date = datetime.strptime(date, "%Y-%m-%d")
     else:
@@ -17,28 +17,25 @@ def get_paths(date: str, raw_data_path: str, dataset: str):
     train_date = processed_date - relativedelta(months=2)
     val_date = processed_date - relativedelta(months=1)
     test_date = processed_date
-    train_path = os.path.join(raw_data_path, f"{dataset}_tripdata_{train_date.year}-{str(train_date.month).zfill(2)}.parquet")
-    val_path = os.path.join(raw_data_path, f"{dataset}_tripdata_{val_date.year}-{str(val_date.month).zfill(2)}.parquet")
-    test_path = os.path.join(raw_data_path, f"{dataset}_tripdata_{test_date.year}-{str(test_date.month).zfill(2)}.parquet")
-    return train_path, val_path, test_path
+    train_filename = f"{dataset}_tripdata_{train_date.year - 1}-{str(train_date.month).zfill(2)}.parquet"
+    val_filename = f"{dataset}_tripdata_{val_date.year - 1}-{str(val_date.month).zfill(2)}.parquet"
+    test_filename = f"{dataset}_tripdata_{test_date.year - 1}-{str(test_date.month).zfill(2)}.parquet"
+    return train_filename, val_filename, test_filename
 
-@task
-def prepare_data(train_path: str, val_path: str, test_path: str, dest_path: str = './output', date: str = None):
-    process_data(train_path, val_path, test_path, dest_path=dest_path, date=date)
-
-@task
-def train_model(dest_path: str = './output', date: str = None):
-    rf = train(dest_path, date)
-    return rf
-
-@flow
+@flow()
 def main(date=None):
-    train_path, val_path, test_path = get_paths(date=date, raw_data_path=RAW_DATA_PATH, dataset='green').result()
-    dv = prepare_data(train_path, val_path, test_path, dest_path=DEST_PATH, date=date).result()
-    rf = train_model(dest_path=DEST_PATH, date=date).result()
+    train_filename, val_filename, test_filename = get_paths(date=date, dataset='green').result()
+    download_dataset_by_date(RAW_DATA_PATH, train_filename, wait_for=[get_paths])
+    download_dataset_by_date(RAW_DATA_PATH, val_filename, wait_for=[get_paths])
+    download_dataset_by_date(RAW_DATA_PATH, test_filename, wait_for=[get_paths])
+    train_path = os.path.join(RAW_DATA_PATH, train_filename)
+    val_path = os.path.join(RAW_DATA_PATH, val_filename)
+    test_path = os.path.join(RAW_DATA_PATH, test_filename)
+    dv = process_data(train_path, val_path, test_path, dest_path=DEST_PATH, date=date, wait_for=[download_dataset_by_date]).result()
+    rf = train(data_path=DEST_PATH, date=date, wait_for=[process_data]).result()
 
 # main()
 
 # test function
-date = '2021-03-01'
+date = '2022-07-17'
 main(date)
